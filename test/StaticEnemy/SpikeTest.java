d8f4331170472c56788f9a9cf228610000ff77b1
Simone Cristofaro <48525265+heisengit@users.noreply.github.com>
2020-01-06 23:23:04 +0100

    Still more test classes

---
+++ test/StaticEnemy/SpikeTest.java
@@ -0,0 +1,85 @@
+package StaticEnemy;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.newdawn.slick.geom.Rectangle;
+import org.newdawn.slick.geom.Shape;
+
+/**
+ *
+ * @author Heisenberg
+ */
+public class SpikeTest {
+
+    private Spike spike;
+    private Shape hitbox;
+    private int x = 0;
+    private int y = 0;
+    private int damage;
+
+    @BeforeClass
+    public static void setUpClass() {
+        System.out.println("----- Test Spike class -----");
+    }
+
+    @Before
+    public void initialize() {
+        /**
+         * Initialize it with some values.
+         * Warning: I'm actually using an abstract class. 
+         */
+        spike = new Spike(x, y, 1) {};
+    }
+
+    @After
+    public void end() {
+    }
+    
+    @Test
+    public void test_differentDamageAccordingToDifficulty(){
+        this.spike = new Spike(0,0,2) {};
+        Assert.assertTrue(spike.doDamage() == 10);
+        
+    }
+
+    @Test
+    public void test_getHitbox() {
+        this.hitbox = spike.getHitbox();
+
+        Assert.assertNotNull("Hitbox returned null", hitbox);
+        Assert.assertEquals(spike.getClass(), Spike.class);
+        Assert.assertEquals(this.hitbox.getClass(), Rectangle.class);
+    }
+
+    @Test
+    public void test_getHitbox_Height() {
+        this.hitbox = spike.getHitbox();
+
+        Assert.assertNotNull("Hitbox returned null", hitbox);
+        Assert.assertEquals(spike.getClass(), Spike.class);
+        Assert.assertEquals(this.hitbox.getClass(), Rectangle.class);
+        Assert.assertTrue(hitbox.getHeight() == 20);
+    }
+
+    @Test
+    public void test_getHitbox_Width() {
+        this.hitbox = spike.getHitbox();
+
+        Assert.assertNotNull("Hitbox returned null", hitbox);
+        Assert.assertEquals(spike.getClass(), Spike.class);
+        Assert.assertEquals(this.hitbox.getClass(), Rectangle.class);
+        Assert.assertTrue(hitbox.getWidth() == 20);
+    }
+
+    @Test
+    public void test_doDamage() {
+        this.damage = spike.doDamage();
+
+        Assert.assertNotNull(damage);
+        Assert.assertTrue("Is positive", damage > 0);
+        Assert.assertTrue("Is actually equal to 10, since i passed 2 as difficulty.", damage == 5);
+    }
+}
