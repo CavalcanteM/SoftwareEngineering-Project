442402848e21f1e127df8ea6702d8f4cdd68ba95
Adria <adriano.luongo1@studenti.unicampania.it>
2020-01-07 14:45:54 +0100

    New levels and new tilesetmap

---
+++ src/map_level/TileSet_map_3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.1" name="TileSet_map_3" tilewidth="30" tileheight="30" tilecount="80" columns="5">
+ <image source="../Documenti/NetBeansProjects/SEProject/SEProject/src/map_level/TileSet_map_3.png" width="150" height="480"/>
+</tileset>
