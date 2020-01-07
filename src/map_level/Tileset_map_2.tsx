9d7b2a96273a781efa55f7e5ded03e01dc7da9d4
Simone Cristofaro <48525265+heisengit@users.noreply.github.com>
2020-01-07 15:11:24 +0100

    Added final world levels and the changing color of the tilset map

---
+++ src/map_level/Tileset_map_2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.1" name="Tileset_map_2" tilewidth="30" tileheight="30" tilecount="80" columns="5">
+ <image source="TileSet_map_2.png" width="150" height="480"/>
+</tileset>
